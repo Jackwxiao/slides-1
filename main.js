b833311a0f0b716a6dda464d1e682fa9b2fafd24
wuxiao <1291383707@qq.com>
2019-08-09 20:22:23 +0800

    版本二

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
-let n=1
+let n
+init()
 setInterval(()=>{
-    $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave').one('transitionend',(e)=>{
-        $(e.currentTarget).removeClass('leave').addClass('enter')
+    makeLeave(getImage(n)).one('transitionend',(e)=>{
+        makeEnter($(e.currentTarget))
     })
-    $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+
+    makeCurrent(getImage(n+1))
     n += 1
-},3000)
+},1500)
+
+
+function init(){
+    n = 1
+    $(`.images > img:nth-child(${n})`).addClass('current')
+    .siblings().addClass('enter')
+}
 
 function x(n){
     if(n>3){
-        n = n%3
+        n=n%3
         if(n===0){
-            n = 3
+            n=3
         }
     }
     return n
+}
+
+function getImage(n){
+   return $(`.images > img:nth-child(${x(n)})`)
+}
+
+function makeCurrent($node){
+    return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node){
+    return $node.removeClass('enter current').addClass('leave')
+}
+function makeEnter($node){
+    return $node.removeClass('current leave').addClass('enter')
 }
